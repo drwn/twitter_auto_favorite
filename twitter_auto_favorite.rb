a5100b5398856bc88b704eeda77c9a3588433543
root <root@debian>
2015-01-20 16:38:04 +0000

    yes hello

---
+++ twitter_auto_favorite.rb
@@ -0,0 +1,30 @@
+require 'twitter' #gem install twitter
+while true
+	begin
+		# Create a read write application from : 
+		# https://apps.twitter.com
+		# authenticate it for your account
+		# fill in the following
+		config = {
+			consumer_key:        '',
+			consumer_secret:     '',
+			access_token:        '',
+			access_token_secret: ''
+		}
+		rClient = Twitter::REST::Client.new config
+		sClient = Twitter::Streaming::Client.new(config)
+
+		# topics to watch
+		topics = ['#topichere']
+		sClient.filter(:track => topics.join(',')) do |tweet|
+			if tweet.is_a?(Twitter::Tweet)
+			  puts tweet.text 
+			  rClient.fav tweet
+			end
+		end
+	rescue
+		puts 'error occurred, waiting for 5 seconds'
+		sleep 5
+	end
+
+end
